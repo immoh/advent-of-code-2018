62194e3d25db77553a573f4a2ae0b0196b6d9eb6
Immo Heikkinen <immo.heikkinen@gmail.com>
2018-12-09 17:07:17 +0200

    Day 9, part 2

--- src/day09.clj
+++ src/day09.clj
@@ -1,32 +1,62 @@
 (ns day09
   (:require [clojure.string]))
 
+(defprotocol INode
+  (get-value [_])
+  (get-next-node [_])
+  (set-next-node! [_ ^INode node])
+  (get-prev-node [_])
+  (set-prev-node! [_ ^INode node]))
+
+(deftype Node [value ^:volatile-mutable prev ^:volatile-mutable next]
+  INode
+  (get-value [_] value)
+  (get-next-node [_] next)
+  (set-next-node! [_ node] (set! next node))
+  (get-prev-node [_] prev)
+  (set-prev-node! [_ node] (set! prev node)))
+
+(defn ->circle [value]
+  (let [node (->Node value nil nil)]
+    (set-prev-node! node node)
+    (set-next-node! node node)
+    node))
+
+(defn add-value-to-circle [circle value]
+  (let [node (get-next-node circle)
+        next-node (get-next-node node)
+        new-node (->Node value node next-node)]
+    (set-next-node! node new-node)
+    (set-prev-node! next-node new-node)))
+
+(defn remove-node [node]
+  (let [prev-node (get-prev-node node)
+        next-node (get-next-node node)]
+    (set-next-node! prev-node next-node)
+    (set-prev-node! next-node prev-node)
+    next-node))
+
 (defn parse-input [input]
   (map #(Integer/parseInt %) (re-seq #"\d+" input)))
 
-(defn play-turn [{:keys [scores circle current]} [player marble]]
+(defn play-turn [{:keys [scores circle]} [player marble]]
   (if (zero? (mod marble 23))
-    (let [index-to-remove (mod (- current 7) (count circle))
-          marble-to-remove (get circle index-to-remove)]
-      {:circle  (vec (concat (take index-to-remove circle) (drop (inc index-to-remove) circle)))
-       :current index-to-remove
-       :scores  (update scores player (fnil + 0) marble marble-to-remove)})
-    (let [new-index (inc (mod (inc current) (count circle)))]
-      {:circle  (vec (concat (take new-index circle) [marble] (drop new-index circle)))
-       :current new-index
-       :scores  scores})))
+    (let [removable-node (nth (iterate get-prev-node circle) 7)]
+      {:circle  (remove-node removable-node)
+       :scores  (update scores player (fnil + 0) marble (get-value removable-node))})
+    {:circle  (add-value-to-circle circle marble)
+     :scores  scores}))
 
 (defn play-game [players last-marble]
   (reduce
     play-turn
     {:scores {}
-     :circle [0]
-     :current 0}
+     :circle (->circle 0)}
     (map vector
          (cycle (range 1 (inc players)))
          (range 1 (inc last-marble)))))
 
-(defn part1 [input]
+(defn solve [input]
   (let [[players last-marble] (parse-input input)]
     (-> (play-game players last-marble)
         (:scores)
