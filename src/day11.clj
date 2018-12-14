7e92ec081ff4da6d7eb134320216fcbd8a7628a6
Immo Heikkinen <immo.heikkinen@gmail.com>
2018-12-14 20:16:34 +0200

    Trail Day 11 part 2

--- src/day11.clj
+++ src/day11.clj
@@ -1,19 +1,35 @@
 (ns day11
   (:require [clojure.string]))
 
-(defn calculate-total-power [power-level-map [x y] d]
-  (reduce + (for [x' (range x (+ x d))
-                  y' (range y (+ y d))]
-              (get power-level-map [x' y']))))
-
-(defn create-total-power-map [power-level-map]
+(defn max-by [f coll]
   (reduce
-    (fn [m p]
-      (assoc m p (calculate-total-power power-level-map p 3)))
-    {}
-    (for [x (range 1 299)
-          y (range 1 299)]
+    (fn [{:keys [max-value] :as current-max} item]
+      (let [value (f item)]
+        (if (> value max-value)
+          {:max-value value
+           :max-item  item}
+          current-max)))
+    {:max-value (f (first coll))
+     :max-item  (first coll)}
+    (rest coll)))
+
+(defn total3x3 [power-level-sum-map [x y]]
+  (reduce
+    +
+    (map
+      (fn [ys]
+        (- (nth ys (+ y 2)) (nth ys (dec y) 0)))
+      (take 3 (drop x power-level-sum-map)))))
+
+(defn find-maximum-3x3-square [power-level-sum-map]
+  (max-by
+    (partial total3x3 power-level-sum-map)
+    (for [x (range 0 298)
+          y (range 0 298)]
       [x y])))
+
+(defn create-power-level-sum-map [power-level-map]
+  (map (partial reductions +) power-level-map))
 
 (defn hundreds-digit [n]
   (mod (int (/ n 100)) 10))
@@ -27,18 +43,47 @@
         (- 5))))
 
 (defn create-power-level-map [grid-serial-number]
-  (reduce
-    (fn [m [x y]]
-      (assoc m [x y] (calculate-power-level x y grid-serial-number)))
-    {}
-    (for [x (range 1 301)
-          y (range 1 301)]
-      [x y])))
+  (mapv (fn [x]
+          (mapv (fn [y]
+                  (calculate-power-level x y grid-serial-number))
+                (range 1 301)))
+        (range 1 301)))
 
 (defn part1 [grid-serial-number]
   (->> (create-power-level-map grid-serial-number)
-       (create-total-power-map)
-       (sort-by val)
+       (create-power-level-sum-map)
+       (find-maximum-3x3-square)
+       (map inc)
+       (clojure.string/join ",")))
+
+(defn totalnxn [power-level-sum-map [x y n]]
+  (reduce
+    +
+    (map
+      (fn [ys]
+        (- (nth ys (+ y n -1)) (nth ys (dec y) 0)))
+      (take n (drop x power-level-sum-map)))))
+
+(defn find-maximum-nxn-square-at-position [power-level-sum-map [x y]]
+  (max-by
+    #(totalnxn power-level-sum-map [x y %])
+    (range 1 (- 301 (max x y)))))
+
+(defn find-maximum-nxn-square [power-level-sum-map]
+  (->> (pmap (partial find-maximum-nxn-square-at-position power-level-sum-map)
+             (for [x (range 0 298)
+                   y (range 0 298)]
+               [x y]))
+       (sort-by :max-value)
        (last)
-       (key)
+       (:max-item)))
+
+(defn fix-coordinates [[x y n]]
+  [(inc x) (inc y) n])
+
+(defn part2 [grid-serial-number]
+  (->> (create-power-level-map grid-serial-number)
+       (create-power-level-sum-map)
+       (find-maximum-nxn-square)
+       (fix-coordinates)
        (clojure.string/join ",")))
